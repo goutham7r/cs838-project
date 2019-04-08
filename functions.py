e3952db92482c7b220a6cbc3e6e3efe42a718594
thumbe4 <thumbevarun@gmail.com>
2019-04-08 02:56:38 -0500

    Age gender script json

--- functions.py
+++ functions.py
@@ -3,27 +3,51 @@ from datetime import datetime
 import bs4  as BS
 import re
 
+#two calcAge functions in case one of them doesn't work
+def calcAge2(name):
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        prefix='http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles='
+        suffix='&format=xml'
+        infile = opener.open(prefix+str(name)+suffix)
+        page2 = infile.read()
+        soup = BS.BeautifulSoup(page2)
 
+        birth_re = re.search(r'(Birth date(.*?)}})', soup.getText(),flags=re.I)
+        birth_data = birth_re.group(0).split('|')
+        try:
+            year = int(birth_data[1])
+        except:
+            year = int(birth_data[2])
+        today = datetime.today()
+        age=today.year-year
+        return age
+    except:
+        return None
 # name parameter has to be of the form first_name_last_name
-
 def calcAge(name):
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    prefix='http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles='
-    suffix='&format=xml'
-    infile = opener.open(prefix+name+suffix)
-    page2 = infile.read()
-    soup = BS.BeautifulSoup(page2)
-    #print soup.prettify()
-    birth_re = re.search(r'(Birth date(.*?)}})', soup.getText())
-    birth_data = birth_re.group(0).split('|')
-    year = int(birth_data[2])
-    today = datetime.today()
-    age=today.year-year
-    return age
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        prefix='https://www.google.com/search?q='
+        infile = opener.open(prefix+str(name))
+        
+        page2 = infile.read()
+        soup = BS.BeautifulSoup(page2)
+        #print soup.prettify()
+        text=''.join([i if ord(i) < 128 else ' ' for i in soup.getText()])
+        birth_re = re.search(r'\(age(.*?)year', text,flags=re.I)
+        age=birth_re.group(0).split(' ')[1]
+        return age
+    except:
+        return None
+
+
 
 def calcGender(name):
     try:
+        name=str(name)
         name=name.split('_')
         urlname=name[0]
         for i in range(1,len(name)):
@@ -45,3 +69,4 @@ def calcGender(name):
     else:
         gender='female'
     return gender    
+
